5f803fb242c0095434003ba357ee7f41e90cd83d
Dan <ochiodhis@gmail.com>
2016-11-02 17:22:42 +0300

    componetized profile

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,92 +1,19 @@
-var UserProfileComponent = React.createClass ({
-	getInitialState: function () {
-		return {
-			data: [],
-			successPost: null
-		};
-	},
-	componentDidMount: function () {
-		this.serverRequest = $.get("", function (profession) {
-			this.setState ({
-			});
-		}.bind(this));
-		$('.page-header h1').text('Post A Job');
-	},
-	componentWillUnMount: function () {
-		this.serverRequest.abort();
-	},
-	onProfessionChange: function ( e ) {
-		this.setState ({selectedProfessionId: e.target.value});
-	},
-	onTitleChange: function ( e ) {
-		this.setState ({title: e.target.value});
-	},
-	onDescriptionChange: function ( e ) {
-		this.setState ({
-			description: e.target.value
-		});
-	},
-	onStartChange: function ( e ) {
-		this.setState ({
-			start: e.target.value
-		});
-	},
-	onEndChange: function ( e ) {
-		this.setState ({end: e.target.value});
-	},
-	onBudgetChnage: function ( e ) {
-		this.setState ({
-			budget: e.target.value
-		});
-	},
-	onSave: function ( e ) {
-		$.post("", {
-			profession_id: this.state.selectedProfessionId,
-			title: this.state.title,
-			description: this.state.description,
-			start: this.state.start,
-			end: this.state.end,
-			budget: this.state.budget
-		},
-		function (res) {
-			this.setState ({successPost: res});
-			this.setState ({title: ""});
-			this.setState ({description: ""});
-			this.setState ({start: ""});
-			this.setState ({end: ""});
-			this.setState ({budget: ""});
-			this.setState ({selectedProfessionId: -1});
-		}.bind(this));
-		e.preventDefault ();
-	},
-	render: function () {
-		// var professionOptions = this.state.profession.map ( function ( profession ) {
-		// 	return (
-		// 		<option key={profession.id} value={profession.id} > {profession.title} </option>
-		// 	);
-		// });
+var BackComponent = React.createClass({
+	render: function() {
 		return (
 			<div>
-			{
-				this.state.successPost == "true" ?
-				<div className='alert alert-success'>
-				Job posting was successful.
-				</div>
-				:null
-			}
-			{
-				this.state.successPost == "false" ?
-				<div className='alert alert-danger'>
-				Sorry! Job posting failed. Please try again.
-				</div>
-				: null
-			}
-			<a href='/nyumbani/index.php/timeline/'
-			className='btn btn-raised btn-info margin-bottom-1em'>
-			Back to Search
-			</a>
-			<form onSubmit={this.onSave}>
-				<div className="card">
+			   	<a href='/nyumbani/index.php/timeline/'
+			   		//onClick={() => this.props.changeAppMode('search')}
+					className='btn btn-raised btn-info margin-bottom-1em' ref = "back">
+					Back to Search
+				</a>
+			</div>
+			);
+	}
+});
+var TabComponent = React.createClass({
+	render: function() {
+		return (
 					<ul className="nav nav-tabs" role="tablist">
 						<li className="active">
 							<a data-toggle="tab" href="#basic" role="tab">
@@ -104,67 +31,81 @@ var UserProfileComponent = React.createClass ({
 							</a>
 						</li>
 					</ul>
+			
+			);
+	}
+});
+var TabContentComponent = React.createClass({
+	render: function () {
+		return (
 					<div className="tab-content">
+					</div>
+		);
+	}
+});
+var BasicDetailsComponent = React.createClass({
+	render: function () {
+		return (
 						<div className="tab-pane active" id="basic" role="tabpanel">
 							<div className="col-md-6">
 							<div className="col-md-4">
 								<div className="form-group label-floating">
 				                        <label className="control-label" htmlFor="email">E-mail</label>
-				                        <input type="text" id="email" name="email" className="form-control form-control-sm" value={this.state.email_add} />
+				                        <input type="text" id="email" name="email" className="form-control form-control-sm" />
 								</div>
 							</div>
 							<div className="col-md-4">
 								<div className="form-group label-floating">
 				                        <label className="control-label" htmlFor="phone">Phone Number</label>
-				                        <input type="tel" id="phone" name="phone" className="form-control form-control-sm" value={this.state.phone} />
+				                        <input type="tel" id="phone" name="phone" className="form-control form-control-sm" />
 								</div>
 							</div>
 							<div className="col-md-4">
 								<div className="form-group label-floating">
 				                        <label className="control-label" htmlFor="email">Profession</label>
-				                        <input type="text" id="profession" name="profession" className="form-control form-control-sm" value={this.state.email_add} />
+				                        <input type="text" id="profession" name="profession" className="form-control form-control-sm" />
 								</div>
 							</div>
 							<div className="col-md-4">
 								<div className="form-group label-floating">
 				                        <label className="control-label" htmlFor="firstname">First Name</label>
-				                        <input type="text" id="firstname" name="firstname" className="form-control form-control-sm" value={this.state.firstname} />
+				                        <input type="text" id="firstname" name="firstname" className="form-control form-control-sm" />
 								</div>
 							</div>
 							<div className="col-md-4">
 								<div className="form-group label-floating">
 				                        <label className="control-label" htmlFor="lastname">Last Name</label>
-				                        <input type="text" id="lastname" name="lastname" className="form-control form-control-sm" value={this.state.lastname} />
+				                        <input type="text" id="lastname" name="lastname" className="form-control form-control-sm" />
 								</div>
 							</div>	
 							<div className="col-md-4">
 								<div className="form-group label-floating">
 				                        <label className="control-label" htmlFor="surname">Surname</label>
-				                        <input type="text" id="surname" name="surname" className="form-control form-control-sm" value={this.state.surname} />
+				                        <input type="text" id="surname" name="surname" className="form-control form-control-sm" />
 								</div>
 							</div>
 							<div className="col-md-6">
 								<div className="form-group label-floating">
 				                        <label className="control-label" htmlFor="id_pass">ID/Passport</label>
-				                        <input type="text" id="id_pass" name="id_pass" className="form-control" value={this.state.id_pass} />
+				                        <input type="text" id="id_pass" name="id_pass" className="form-control" />
 								</div>
 							</div>
 							<div className="col-md-6">
 								<div className="form-group label-floating">
 				                        <label className="control-label" htmlFor="city">City/Town</label>
-				                        <input type="text" id="city" name="city" className="form-control form-control-sm" value={this.state.city} />
+				                        <input type="text" id="city" name="city" className="form-control form-control-sm" />
 								</div>
 							</div>
 							<div className="col-md-6">
 								<div className="form-group label-floating">
 				                        <label className="control-label" htmlFor="estate">Estate/Locality</label>
-				                        <input type="text" id="estate" name="estate" className="form-control form-control-sm" value={this.state.estate} />
+				                        <input type="text" id="estate" name="estate" className="form-control form-control-sm" />
 								</div>
 							</div>
 							<div className="col-md-6">
 								<div className="form-group">
 				                        <label className="control-label" htmlFor="dob">DOB</label>
-				                        <input type="date" id="dob" name="dob" placeholder="06/25/1980" className="form-control form-control-sm" value={this.state.dob} />
+				                        <input type="date" id="dob" name="dob" placeholder="06/25/1980" className="form-control form-control-sm" />
 								</div>
 							</div>
 							</div>
@@ -190,25 +131,32 @@ var UserProfileComponent = React.createClass ({
 							</div>
 						</div>
 						</div>
+
+		);
+	}
+});
+var EducationBackgroundComponent = React.createClass({
+	render: function () {
+		return (
 						<div className="tab-pane" id="education" role="tabpanel">
 							<div className="col-md-6">
 								<div className="col-md-4">
 									<div className="form-group label-floating">
 					                        <label className="control-label" htmlFor="primary">Prim. Sch./KCPE</label>
-					                        <input type="text" id="primary" name="primary" className="form-control form-control-sm" value={this.state.primary} />
+					                        <input type="text" id="primary" name="primary" className="form-control form-control-sm" />
 									</div>
 								</div>
 								<div className="col-md-4">
 									<div className="form-group label-floating">
 					                        <label className="control-label" htmlFor="grade1">KCPE Grade</label>
-					                        <input type="text" id="grade1" name="grade1" className="form-control form-control-sm" value={this.state.secondary} />
+					                        <input type="text" id="grade1" name="grade1" className="form-control form-control-sm" />
 									</div>
 								</div>
 								<div className="col-md-4">
 									<div className="form-group">
 									<input type="file" id="primary_cert" multiple="" />
 				    				<div className="input-group">
-					                        <input type="text" readOnly="" id="primary_cert" name="primary_cert" className="form-control form-control-sm" value={this.state.primary_cert} placeholder="Attach certificate" />
+					                        <input type="text" readOnly="" id="primary_cert" name="primary_cert" className="form-control form-control-sm" placeholder="Attach certificate" />
 					                        <span className="input-group-btn input-group-sm">
 										      <button type="button" className="btn btn-fab">
 										        <i className="pe-7s-paperclip pe-va pe-lg"></i>
@@ -220,20 +168,20 @@ var UserProfileComponent = React.createClass ({
 								<div className="col-md-4">
 									<div className="form-group label-floating">
 					                        <label className="control-label" htmlFor="secondary">High Sch./KCSE</label>
-					                        <input type="text" id="secondary" name="secondary" className="form-control form-control-sm" value={this.state.secondary} />
+					                        <input type="text" id="secondary" name="secondary" className="form-control form-control-sm" />
 									</div>
 								</div>
 								<div className="col-md-4">
 									<div className="form-group label-floating">
 					                        <label className="control-label" htmlFor="grade">KCSE Grade</label>
-					                        <input type="text" id="grade" name="grade" className="form-control form-control-sm" value={this.state.secondary} />
+					                        <input type="text" id="grade" name="grade" className="form-control form-control-sm"  />
 									</div>
 								</div>
 								<div className="col-md-4">
 									<div className="form-group">
 									<input type="file" id="primary_cert" multiple="" />
 				    				<div className="input-group">
-					                        <input type="text" readOnly="" id="secondary_cert" name="secondary_cert" className="form-control form-control-sm" value={this.state.secondary_cert} placeholder="Attach certificate" />
+					                        <input type="text" readOnly="" id="secondary_cert" name="secondary_cert" className="form-control form-control-sm" placeholder="Attach certificate" />
 					                        <span className="input-group-btn input-group-sm">
 										      <button type="button" className="btn btn-fab">
 										        <i className="pe-7s-paperclip pe-va pe-lg"></i>
@@ -245,20 +193,20 @@ var UserProfileComponent = React.createClass ({
 								<div className="col-md-4">
 									<div className="form-group label-floating">
 					                        <label className="control-label" htmlFor="uni">College/Uni</label>
-					                        <input type="text" id="uni" name="uni" className="form-control form-control-sm" value={this.state.uni} />
+					                        <input type="text" id="uni" name="uni" className="form-control form-control-sm" />
 									</div>
 								</div>
 								<div className="col-md-4">
 									<div className="form-group label-floating">
 					                        <label className="control-label" htmlFor="grade2">Honors/GPA</label>
-					                        <input type="text" id="grade2" name="grade2" className="form-control form-control-sm" value={this.state.uni} />
+					                        <input type="text" id="grade2" name="grade2" className="form-control form-control-sm" />
 									</div>
 								</div>
 								<div className="col-md-4">
 									<div className="form-group">
 									<input type="file" id="uni_cert" multiple="" />
 				    				<div className="input-group">
-					                        <input type="text" readOnly="" id="uni_cert" name="uni_cert" className="form-control form-control-sm" value={this.state.uni_cert} placeholder="Attach Degree/Diploma" />
+					                        <input type="text" readOnly="" id="uni_cert" name="uni_cert" className="form-control form-control-sm" placeholder="Attach Degree/Diploma" />
 					                        <span className="input-group-btn input-group-sm">
 										      <button type="button" className="btn btn-fab">
 										        <i className="pe-7s-paperclip pe-va pe-lg"></i>
@@ -272,7 +220,7 @@ var UserProfileComponent = React.createClass ({
 									<div className="form-group label-floating">
 										<div className="input-group">
 					                        <label className="control-label" htmlFor="past_job">Past Employment</label>
-					                        <input type="text" id="past_job" name="past_job" className="form-control form-control-sm" value={this.state.past_job} />
+					                        <input type="text" id="past_job" name="past_job" className="form-control form-control-sm" />
 					                        <span className="input-group-btn input-group-sm">
 										      <button type="button" className="btn btn-fab btn-fab-sm">
 										        <i className="pe-7s-plus pe-va pe-lg"></i>
@@ -285,19 +233,26 @@ var UserProfileComponent = React.createClass ({
 							</div>
 								</div>
 						</div>
+
+		);
+	}
+});
+var SkillsComponent = React.createClass({
+	render: function () {
+		return (
 						<div className="tab-pane" id="skills" role="tabpanel">
 							<div className="col-md-12">
 								<div className="col-md-6">
 									<div className="form-group label-floating">
 					                        <label className="control-label" htmlFor="skill">Add Skill</label>
-					                        <input type="text" id="skill" name="skill" className="form-control form-control-sm" value={this.state.past_job} />
+					                        <input type="text" id="skill" name="skill" className="form-control form-control-sm" />
 									</div>
 									</div>
 								<div className="col-md-6">
 									<div className="form-group label-floating">
 										<div className="input-group">
 					                        <label className="control-label" htmlFor="mode">Acquisition Mode</label>
-					                        <input type="text" id="mode" name="mode" className="form-control form-control-sm" value={this.state.past_job} />
+					                        <input type="text" id="mode" name="mode" className="form-control form-control-sm" />
 					                        <span className="input-group-btn input-group-sm">
 										      <button type="button" className="btn btn-fab btn-fab-sm">
 										        <i className="pe-7s-plus pe-va pe-lg"></i>
@@ -311,6 +266,30 @@ var UserProfileComponent = React.createClass ({
 									</div>
 							</div>
 						</div>
+
+		);
+	}
+});
+var ProfileComponent = React.createClass ({
+	getInitialState: function() {
+		return { }
+	},
+	componentWillMount: function () {
+
+	},
+	componentWillUpdate: function () {
+
+	},
+	render: function () {
+		return (
+			<div>
+			<form onSubmit={this.onSave}>
+				<TabComponent /> 
+				<div className="card">
+					<div className="tab-content">
+					<BasicDetailsComponent />
+					<EducationBackgroundComponent />
+					<SkillsComponent />
 					</div>
 				</div>
 			</form>
@@ -318,4 +297,4 @@ var UserProfileComponent = React.createClass ({
 		);
 	}
 });
-ReactDOM.render(<UserProfileComponent />, document.getElementById('component'));
+ReactDOM.render(<ProfileComponent />, document.getElementById('component'));
