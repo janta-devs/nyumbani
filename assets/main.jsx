3d26890a9234828242b10ee16ba1151af96f2235
Anto-Linux <jadzantonyngayo@gmail.com>
2016-11-15 00:19:42 +0300

    rearranged the file structure in the assets folder

--- assets/main.jsx
+++
@@ -1,9 +0,0 @@
-var $ = require('jquery');
-var React = require('react');
-var ReactDOM = require('react-dom');
-var MainApp = require('./components/MainApp.jsx');
-var bootstrap = require('bootstrap');
-var mdl = require('material-design-lite');
-
-
-ReactDOM.render(<MainApp />, document.getElementById('component'));
